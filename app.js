96eb4fd5d19f746f267c34b1ac9e5aa6332f9f0a
priyanshi1811 <priyanhigupta1811@gmail.com>
2024-06-28 22:52:38 +0530

    Episode-3 done

--- app.js
+++ app.js
@@ -1,16 +1,38 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./index.css";
 
-const heading = React.createElement("div", { id: "greeting1" }, [
-  React.createElement("div", { id: "greeting" }, [
-    React.createElement("h1", { id: "ram" }, "I'm Ram1"),
-    React.createElement("h2", { id: "ram" }, "I'm Ram2"),
-  ]),
-  React.createElement("div", { id: "greeting" }, [
-    React.createElement("h1", { id: "ram" }, "I'm Ram1"),
-    React.createElement("h2", { id: "ram" }, "I'm Ram2"),
-  ]),
-]);
+// React Element (object)
 
+//React.createElement => ReactElement ->js Object -> HTMlElement(render)
+const heading = React.createElement("h1", { id: "heading" }, "Hello here");
+
+// jsx -> transpiled before it reaches to JS - parcel - Babel(tranpiler (jsx->react))
+//jsx element is also object end of the day
+//JSX -> React.createElement->ReactElement ->js Object -> HTMlElement(render)
+const jsxHeading = <h1 id="heading">Hello i'm jsx</h1>;
+
+const Title = () => {
+  return <h1>I'm title</h1>;
+};
+
+//Read about Babel
+
+//Attribute will be in camel case only
+
+const HeadingComponent = () => {
+  return (
+    <div>
+      {Title()}
+      <Title />
+      <Title></Title>
+      {/* all above 3 statement are same they will include title component here */}
+      {jsxHeading}
+      {/* {} -> this excutes any piece of js variable or console.log */}
+      <h1 className="heading">I'm heading</h1>;
+    </div>
+  );
+};
 const root = ReactDOM.createRoot(document.getElementById("div"));
-root.render(heading);
+
+root.render(<HeadingComponent />);
