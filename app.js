03168d1d3fe684b87831b5994704907469288762
priyanshi1811 <priyanhigupta1811@gmail.com>
2024-06-29 12:33:32 +0530

    episode-4

--- app.js
+++ app.js
@@ -1,16 +1,81 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import logo from "./assets/logo.png";
+import "./index.css";
 
-const heading = React.createElement("div", { id: "greeting1" }, [
-  React.createElement("div", { id: "greeting" }, [
-    React.createElement("h1", { id: "ram" }, "I'm Ram1"),
-    React.createElement("h2", { id: "ram" }, "I'm Ram2"),
-  ]),
-  React.createElement("div", { id: "greeting" }, [
-    React.createElement("h1", { id: "ram" }, "I'm Ram1"),
-    React.createElement("h2", { id: "ram" }, "I'm Ram2"),
-  ]),
-]);
+//Planning
+
+/*Headers
+ -logo
+ -Nav item
+Body
+ -search,
+ -RestaurantContainer
+   -RestaurandCard
+Footer
+ -Copyrights
+ -Links
+ -Address/contacts
+ */
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img className="logo" src={logo} />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const RestaurantCard = () => {
+  return (
+    <div className="res-card">
+      <div className="res-image-container">
+        <img className="logo" src={logo} />
+      </div>
+      <h3>Meghna Food</h3>
+      <h4>burger, pizza, perry phery</h4>
+      <h4>400 price</h4>
+      <h4>4.3 star</h4>
+      <h4>35 minutes</h4>
+    </div>
+  );
+};
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="res-contaier">
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+      </div>
+    </div>
+  );
+};
+
+const Footer = () => {};
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("div"));
-root.render(heading);
+
+root.render(<AppLayout />);
